6f546b69b9c0cd87ca65c69b3233ceaeeb558b63
fyun89 <fyun89@gmail.com>
2018-05-06 13:44:04 -0700

    Completed 6ee6ee6ee

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,66 @@
-class Bee {
+class Bee extends Grub { //inherit from other object
   // TODO..
+  constructor(){
+  	super()
+  	this.age = 5;
+  	this.color = "yellow"
+  	this.job = "Keep on growing"
+  }
 };
+
+
+/*
+Grub
+
+Create a Grub class, in ES6 style, with:
+an age property that is set to 0
+a color property that is setto pink
+a food property that is set to jelly
+an eat method that returns 'Mmmmmmmmm jelly'
+Bee
+
+Create a Bee class, in ES6 style, with:
+the Grub superclass
+an age property that is set to 5
+a color property that is set to yellow
+a food property that is inherited from grub
+an eat method that is inherited from grub
+a job property that is set to Keep on growing
+HoneyMakerBee
+
+Create a HoneyMakerBee class, in ES6 style, with:
+the Bee superclass
+an age property that is set to 10
+a job property that is set to make honey
+a color property inherited from bee that is set to yellow
+a food property that is inherited from grub
+an eat method that is inherited from grub
+a honeyPot property that is set to 0
+a makeHoney method that adds 1 to that honeyBee's honeyPot
+a giveHoney method that subtracts 1 from that honeyBee's honeyPot
+ForagerBee
+
+Create a ForagerBee class, in ES6 style, with:
+the Bee superclass
+an age property that is set to 10
+a job property that is set to find pollen
+a color property inherited from bee that is set to yellow
+a food property that is inherited from grub
+an eat method that is inherited from grub
+a canFly property that is set true
+a treasureChest property that is set to an empty array []
+a forage method that allows the bee to add a treasure to the treasureChest
+RetiredForagerBee
+
+Create a RetiredForagerBee class, in ES6 style, with:
+the ForagerBee superclass
+an age property that is set to 40
+a job property that is set to gamble
+a canFly property that is set to false
+a color property that is set to grey
+a forage method that returns I am too old, let me play cards instead
+a food property that is inherited from grub
+an eat method that is inherited from grub
+a treasureChest property inherited from ForagerBee that is set to an empty array []
+an always winning gamble method that allows the bee to add a treasure to the treasureChest
+*/
--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,18 @@
-class RetiredForagerBee {
+class RetiredForagerBee extends ForagerBee {
   // TODO..
+  //console.log('before constructor: ' + this)
+  constructor() {
+  	//console.log('before super: ' + this)
+  	super();
+  	this.age = 40;
+  	this.job = 'gamble';
+  	this.canFly = false;
+  	this.color = 'grey';
+  	this.forage = function() {
+  		return "I am too old, let me play cards instead"
+  	};
+  	this.gamble = function() {
+  		this.treasureChest.push('treasure')
+  	};
+  };
 };
